5f80b5d4377e6c36328e4610da95a5ff7426b0b9
Ramiro Rivera <ramiro@miro.com>
2023-07-25 18:29:32 +0200

    chore: update rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,29 +2,41 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:import/recommended', 'eslint-config-prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'eslint-config-prettier',
+    'plugin:storybook/recommended',
+  ],
   settings: {
     react: {
-      version: 'detect'
+      version: 'detect',
     },
     'import/resolver': {
       node: {
         paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-      typescript: {} // this loads <rootdir>/tsconfig.json to eslint
-    }
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
   },
 
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', {
-      allowConstantExport: true
-    }],
-    'react/react-in-jsx-scope': 'off'
-  }
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
 };
